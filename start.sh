5250b3fcd7b8d050acf73da8dc0224b64ba7e46a
Christian Boulanger <info@bibliograph.org>
2015-09-04 15:42:05 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-[ -z $mariapw ] && mariapw="MariaDBIsGoodForYou!"
-service mysql start
-sleep 10
-mysql -uroot -e "UPDATE user SET password=PASSWORD('$mariapw') WHERE user='root';flush privileges;" mysql
-service mysql stop
 supervisord -n
